1ef48deeb09190421ecb2c8ad317e9d7e7b5712e
Сем Елена <semova.s81@bk.ru>
2022-09-08 16:15:05 +0300

    modific1

--- src/main/java/ru/netology/afisha/Afisha.java
+++ src/main/java/ru/netology/afisha/Afisha.java
@@ -38,4 +38,5 @@ public class Afisha {
         }
         return tmp;
     }
+
 }
--- src/test/java/ru/netology/afisha/AfishaTest.java
+++ src/test/java/ru/netology/afisha/AfishaTest.java
@@ -25,6 +25,7 @@ public class AfishaTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
+
     @Test
     public void shouldAfisha10Last() {
         Afisha afisha = new Afisha();
@@ -45,5 +46,55 @@ public class AfishaTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void shouldAfisha11() {
+        Afisha afisha = new Afisha();
+
+        afisha.add("Film 1");
+        afisha.add("Film 2");
+        afisha.add("Film 3");
+        afisha.add("Film 4");
+        afisha.add("Film 5");
+        afisha.add("Film 6");
+        afisha.add("Film 7");
+        afisha.add("Film 8");
+        afisha.add("Film 9");
+        afisha.add("Film 10");
+        afisha.add("Film 11");
+
+        String[] expected = { "Film 11", "Film 10", "Film 9", "Film 8", "Film 7", "Film 6", "Film 5", "Film 4", "Film 3", "Film 2" };
+        String[] actual = afisha.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAfisha6Last() {
+        Afisha afisha = new Afisha();
+
+        afisha.add("Film 5");
+        afisha.add("Film 6");
+        afisha.add("Film 7");
+        afisha.add("Film 8");
+        afisha.add("Film 9");
+        afisha.add("Film 10");
+
+        String[] expected = { "Film 10", "Film 9", "Film 8", "Film 7", "Film 6", "Film 5" };
+        String[] actual = afisha.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAfishaNot() {
+        Afisha afisha = new Afisha();
+
+        String[] expected = {  };
+        String[] actual = afisha.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 
 }
